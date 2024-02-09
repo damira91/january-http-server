84395008ce7163290f8c4a6450ea0662bc16afc4
Damira <arimad1991@gmail.com>
2024-02-09 15:03:59 +0600

    Homework28 is ready

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -3,16 +3,14 @@ package ru.otus.java.basic.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
 
 public class HttpServer {
     static final Logger logger = Logger.getLogger(HttpServer.class.getName());
-    private int port;
-    private Dispatcher dispatcher;
+    private final int port;
+    private final Dispatcher dispatcher;
     // для обработки запросов в отдельных потоках
    // private ExecutorService executor;
 
